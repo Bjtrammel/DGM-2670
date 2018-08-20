2e8ddbe1029356994fcd4731a6c79015c7132d4c
Becky Trammel <10798648@my.uvu.edu>
2018-08-20 14:24:43 -0600

    Created Day 1 project

---
+++ Scripting/Assets/IfStatements.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatement : MonoBehaviour
+{
+
+	public float Speed;
+	public bool Lightson;
+	
+	// Use this for initialization
+	void Start () {
+		if (Lightson && Speed > 4)
+		{
+			//do work
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Scripting/Assets/KillPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+
+
+	public FloatData Health;
+	
+	// Use this for initialization
+	void OnTriggerEnter () {
+		if (Health.Value <= 0)
+		{
+			
+		}
+	}
+	
+}
