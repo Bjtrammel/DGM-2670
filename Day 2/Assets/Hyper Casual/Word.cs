bfafbf6a43ae49acf95e4f8f70fe1da9a9b1ad19
Becky Trammel <10798648@my.uvu.edu>
2018-09-26 14:33:43 -0600

    Started HyperCasual Game

---
+++ Day 2/Assets/Hyper Casual/Word.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Word : MonoBehaviour
+{
+
+	public UnityEvent Solved;
+	
+	public string TheWord;
+	public List<Letter> Value;
+	
+	public void Solving()
+	{
+		foreach (var letter  in TheWord)
+		{
+			//mark as solved. 
+		}
+	}
+}
+
