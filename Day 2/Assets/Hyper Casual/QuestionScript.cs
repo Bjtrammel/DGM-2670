970227e89efb5f951878db10f337cdb8d4b49c7f
Becky Trammel <10798648@my.uvu.edu>
2018-10-14 09:11:20 -0600

    IDK but I made my questions SO and call script

---
+++ Day 2/Assets/Hyper Casual/QuestionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuestionScript : MonoBehaviour
+{
+
+	public Question[] question;
+	private static List<Question> _unanswseredQuestions;
+
+	private Question currentQuestion;
+	
+	void Start ()
+	{
+		if (_unanswseredQuestions == null || _unanswseredQuestions.Count == 0)
+		{
+			_unanswseredQuestions = question.ToList<Questions>();
+		}
+
+		GetRandomQuestion();
+	}
+
+	void GetRandomQuestion()
+	{
+		int randomQuestionIndex = Random.Range(0, _unanswseredQuestions.Count);
+		currentQuestion = _unanswseredQuestions[randomQuestionIndex];
+
+		_unanswseredQuestions.RemoveAt (randomQuestionIndex);
+	}
+
+}
