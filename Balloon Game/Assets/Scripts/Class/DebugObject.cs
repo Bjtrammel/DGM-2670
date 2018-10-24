f83aa356aa8392e1b775b42b50e7a7bf37635c6d
Becky Trammel <10798648@my.uvu.edu>
2018-10-24 16:38:05 -0600

    Got EndGame to work

---
+++ Balloon Game/Assets/Scripts/Class/DebugObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DebugObject : ScriptableObject {
+
+    public void Call(string s)
+    {
+        Debug.Log(s);
+    }
+}
