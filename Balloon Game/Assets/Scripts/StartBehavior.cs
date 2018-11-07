2eb94fdc2c4ca3085a80b6755f3149bc895ed72e
Becky Trammel <10798648@my.uvu.edu>
2018-11-07 14:21:02 -0700

    Created Game Actions

---
+++ Balloon Game/Assets/Scripts/StartBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartBehavior : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	void Start()
+	{
+		Event.Invoke();
+	}
+}
---
+++ Day 2/Assets/Scripts/AnyEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AnyEvent : MonoBehaviour
+{
+
+	public UnityEvent Event;
+	public ActionObject Action;
+	
+	void Start () {
+		Action.Raise += RaiseHandler;
+	}
+
+	private void RaiseHandler()
+	{
+		Event.Invoke();
+	}
+}
